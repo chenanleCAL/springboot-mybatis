ab5f4131dbd3ae1d0a5dd2f998c0c003dc9b5591
chenanle <chenanle@tsfintech.com>
2018-06-11 09:37:43 +0800

    修改文件

--- src/main/java/com/chenanle/controller/protal/UserController.java
+++ src/main/java/com/chenanle/controller/protal/UserController.java
@@ -1,13 +1,25 @@
 package com.chenanle.controller.protal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by chenanle on 2018/6/8.
  */
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/user/")
 public class UserController {
+
+    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping(value = "hello")
+    @ResponseBody
+    public String say() {
+        return "hello springboot!";
+    }
 
 }
